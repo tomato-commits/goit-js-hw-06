cb6283a91f702c3181f47ab565568afdca7dc74e
tomato-commits <arthurpeter7@gmail.com>
2021-10-21 21:25:08 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientElements = [];
+const list = document.getElementById('ingredients');
+
+ingredients.forEach((ingredient) => {
+  const element = document.createElement('li');
+
+  element.textContent = ingredient;
+  element.classList.add('item');
+  ingredientElements.push(element);
+});
+
+list.append(...ingredientElements);

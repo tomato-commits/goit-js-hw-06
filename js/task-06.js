5e4155b023c53956bf8b66effa311e3b670ecd8e
tomato-commits <arthurpeter7@gmail.com>
2021-10-23 21:14:38 +0300

    task 6

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const inputElement = document.getElementById("validation-input");
+const blurListener = () => {
+    const valueLength = inputElement.value.length;
+    const maxLength = +inputElement.dataset.length;
+
+    if (valueLength === maxLength) {
+        inputElement.classList.remove('invalid');
+        inputElement.classList.add('valid');
+    } else {
+        inputElement.classList.remove('valid');
+        inputElement.classList.add('invalid');
+    }
+}
+
+inputElement.addEventListener("blur", blurListener);

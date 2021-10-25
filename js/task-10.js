62dbb035151911cf7ddbfb7e06b68b86e424d13b
tomato-commits <arthurpeter7@gmail.com>
2021-10-25 13:58:41 +0300

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,43 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const boxesContainer = document.getElementById("boxes");
+const input = document.querySelector("#controls input[type='number']");
+let size = 30;
+
+createBtn.addEventListener("click", createBtnHandler);
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBtnHandler() {
+  if (input.value) {
+    const boxes = createBoxes(input.value);
+
+    boxesContainer.append(...boxes);
+  }
+};
+
+function createBoxes(amount) {
+  const boxes = [];
+  
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+
+    boxes.push(div);
+
+    size += 10;
+  }
+
+  return boxes;
+};
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+  size = 30;
+}

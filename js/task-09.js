f8d842c37212a36a7474d0248e18a30b73167e7a
tomato-commits <arthurpeter7@gmail.com>
2021-10-25 13:22:26 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const colorElement = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", handleFunction);
+
+function handleFunction() {
+  const color = getRandomHexColor();
+
+  document.body.style.backgroundColor = color;
+  colorElement.innerHTML = color;
+};

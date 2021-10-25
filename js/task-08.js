ba48726cf880c551ce1202636fff1d684e92543b
tomato-commits <arthurpeter7@gmail.com>
2021-10-25 13:09:31 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    
+    const {elements: {email, password}} = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Please fill in all the fields!");
+    }
+
+    const formData = {
+        email: email.value,
+        password: password.value,
+    };
+
+    console.log('Form data:', formData);
+    
+    event.currentTarget.reset();
+}
